472cf4bc8e20313d6b03654637acf521678aecf9
Szymon Piersiak <piersiak.szymon@gmail.com>
2021-04-16 08:21:38 +0200

    31 stworzenie interfejsu graficznego aplikacji (#4)
    * Stworzenie GUI aplikacji
    
    * Fix
    
    -naprawa błędu gdy użytkownik nie wybrał zdjęcia
    -naprawienie labelki na wynik działania algorytmu

---
+++ GUI.py
@@ -0,0 +1,73 @@
+from tkinter import *
+import Img as img
+
+root = Tk()
+root.title('Dog Breed')
+root.geometry("1920x1080")
+
+# Define background image
+bg = PhotoImage(file="images/bg.png")
+# Create label
+main_label = Label(root, image=bg)
+second_label = Label(root, image=bg)
+
+
+def main_window():
+    main_label.place(x=0, y=0, relwidth=1, relheight=1)
+    # Create text
+    banner_text = Label(main_label, text="You don't know what breed you dog is? Check it out now!",
+                        font=("SimSun", 50), fg="black", bg="#f4ead9")
+    banner_text.place(relx=0.5, rely=0.1, anchor="center")
+
+    # Create button
+    take_photo_button = Button(main_label, text="Insert a picture of your dog.", font=("Segoe UI", 50), bg='#fffff5',
+                               activebackground='#fffff5', command=move_to_second_label)
+    take_photo_button.place(relx=0.5, rely=0.75, anchor="center")
+
+    main_label.pack()
+    root.mainloop()
+
+
+def move_to_second_label():
+    # Take image form user class
+    z = img.upload_image()
+    if z is not None:
+        main_label.pack_forget()
+        second_label.pack()
+        second_window(z)
+
+
+def move_to_first_label():
+    second_label.pack_forget()
+    main_label.pack()
+    main_window()
+
+
+def second_window(im):
+    second_label.place(x=0, y=0, relwidth=1, relheight=1)
+    # Create text
+    b_text = Label(second_label, text="Your dog is ...",
+                   font=("SimSun", 50), fg="black", bg="#faf0de")
+    b_text.place(relx=0.75, rely=0.1, anchor="center")
+
+    # Place for image
+    image_holder = Label(second_label, image=im, height=330, width=330)
+    image_holder.image = im
+    image_holder.place(relx=0.2, rely=0.5, anchor="center")
+
+    # Text attribute - a place for the answer from the algorithm
+    a_text = Label(second_label, text="",
+                    font=("SimSun", 50), fg="black", bg="#fdf3e1")
+    a_text.place(relx=0.60, rely=0.5, anchor="w")
+
+    # Check button
+    check_button = Button(second_label, text="Check it out.", font=("Segoe UI", 50), bg='#fffff5',
+                          activebackground='#fffff5')
+    check_button.place(relx=0.71, rely=0.79, anchor="center")
+    # Back Button
+    back_button = Button(second_label, text="Back", font=("Segoe UI", 40), bg='#fffff5',
+                         activebackground='#fffff5', command=move_to_first_label)
+    back_button.place(relx=0.1, rely=0.07, anchor="center")
+    second_label.pack()
+
+
