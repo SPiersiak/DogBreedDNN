94e91c2f739eee71b6386595a93a0504f48f377d
LipaMar <marcin.lipa@onet.pl>
2021-05-13 21:15:28 +0200

    Dodanie funkcji get_features

--- training.py
+++ training.py
@@ -4,6 +4,11 @@ import time
 import tqdm
 import cv2
 import Img
+import tensorflow as tf
+import gc
+
+from keras.models import Model
+from keras.layers import GlobalAveragePooling2D, Lambda, Input
 
 data_dir = './input'
 data_df = pd.read_csv(os.path.join(data_dir, 'labels.csv'))
@@ -24,3 +29,39 @@ for image in tqdm.tqdm(images_list[:-1]):
     res_image = cv2.resize(orig_image, Img.DEFAULT_RESOLUTION)
     X.append(orig_image)
     Y.append(cls_index)
+
+# FEATURE EXTRACTION OF TRAINING ARRAYS
+AUTO = tf.data.experimental.AUTOTUNE
+
+
+def get_features(model_name, data_preprocessor, data):
+    """
+    1- Create a feature extractor to extract features from the data.
+    2- Returns the extracted features and the feature extractor.
+    """
+    dataset = tf.data.Dataset.from_tensor_slices(data)
+
+    def preprocess(x):
+        x = tf.image.random_flip_left_right(x)
+        x = tf.image.random_brightness(x, 0.5)
+        return x
+
+    ds = dataset.map(preprocess, num_parallel_calls=AUTO).batch(64)
+
+    input_size = data.shape[1:]
+
+    input_layer = Input(input_size)
+    preprocessor = Lambda(data_preprocessor)(input_layer)
+
+    base_model = model_name(weights='imagenet', include_top=False,
+                            input_shape=input_size)(preprocessor)
+
+    avg = GlobalAveragePooling2D()(base_model)
+    feature_extractor = Model(inputs=input_layer, outputs=avg)
+
+    feature_maps = feature_extractor.predict(ds, verbose=1)
+
+    del (feature_extractor, base_model, preprocessor, dataset)
+    gc.collect()
+
+    return feature_maps
