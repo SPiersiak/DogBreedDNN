b9d6a5edf6c3322afe2032189c753bcc60632841
Szymon <piersiak.szymon@gmail.com>
2021-05-13 20:15:23 +0200

    zmiana listy w tablice, One Hot encoding the classes dogs

--- training.py
+++ training.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import os
+import gc
 import time
 import tqdm
 import cv2
 import Img
+import numpy as np
+import keras.utils as ku
 
 data_dir = './input'
 data_df = pd.read_csv(os.path.join(data_dir, 'labels.csv'))
@@ -14,6 +17,7 @@ time.sleep(1)
 images_list = sorted(os.listdir(os.path.join(data_dir, 'train')))
 X = []
 Y = []
+i = 0
 
 for image in tqdm.tqdm(images_list[:-1]):
     cls_name = data_df[data_df['id'] == image[:-4]].iloc[0, 1]
@@ -24,3 +28,15 @@ for image in tqdm.tqdm(images_list[:-1]):
     res_image = cv2.resize(orig_image, Img.DEFAULT_RESOLUTION)
     X.append(orig_image)
     Y.append(cls_index)
+    i += 1
+
+# print(len(X), len(Y))
+Xarr = np.array(X)
+Yarr = np.array(Y).reshape(-1, 1)
+
+del(X)
+# print(Xarr.shape, Yarr.shape)
+gc.collect()
+
+Yarr_hot = ku(Y)
+# print(Xarr.shape, Yarr.shape)
