be572354fb8ddd858598d20b0980c6780d0f7bd0
IrekPrzybylo <50295266+IrekPrzybylo@users.noreply.github.com>
2021-06-10 23:01:39 +0200

    Trenowanie modelu (#13)
    Ok

--- training.py
+++ training.py
@@ -1,18 +1,28 @@
-import pandas as pd
+# for garbage collection
+import gc
+
+# for warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# utility libraries
 import os
-import gc
-import time
+import copy
 import tqdm
-import cv2
-import Img
 import numpy as np
-import keras.utils as ku
-
+import pandas as pd
+import cv2, random, time, shutil, csv
 import tensorflow as tf
-import gc
+import math
+import Img
 
+# keras libraries
+import keras
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
-from keras.layers import GlobalAveragePooling2D, Lambda, Input
+from keras.layers import BatchNormalization, Dense, GlobalAveragePooling2D, Lambda, Dropout, InputLayer, Input
+from keras.utils import to_categorical
+from keras import backend as K
 
 data_dir = './input'
 data_df = pd.read_csv(os.path.join(data_dir, 'labels.csv'))
@@ -30,22 +40,22 @@ for image in tqdm.tqdm(images_list[:-1]):
     cls_index = int(class_names.index(cls_name))
 
     image_path = os.path.join(data_dir, 'train', image)
-    orig_image = cv2.imread(image_path)
+    orig_image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
     res_image = cv2.resize(orig_image, Img.DEFAULT_RESOLUTION)
-    X.append(orig_image)
+    X.append(res_image)
     Y.append(cls_index)
     i += 1
 
-# print(len(X), len(Y))
+print(len(X), len(Y))
 Xarr = np.array(X)
 Yarr = np.array(Y).reshape(-1, 1)
 
 del (X)
-# print(Xarr.shape, Yarr.shape)
+print(Xarr.shape, Yarr.shape)
 gc.collect()
 
-Yarr_hot = ku(Y)
-# print(Xarr.shape, Yarr.shape)
+Yarr_hot = to_categorical(Y)
+print(Xarr.shape, Yarr.shape)
 
 
 # FEATURE EXTRACTION OF TRAINING ARRAYS
@@ -109,6 +119,23 @@ def get_valfeatures(model_name, data_preprocessor, data):
     # print('Feature maps shape: ', feature_maps.shape)
     return feature_maps
 
+# RETURNING CONCATENATED FEATURES USING MODELS AND PREPROCESSORS
+def get_concat_features(feat_func, models, preprocs, array):
+    print(f"Beggining extraction with {feat_func.__name__}\n")
+    feats_list = []
+
+    for i in range(len(models)):
+        print(f"\nStarting feature extraction with {models[i].__name__} using {preprocs[i].__name__}\n")
+        # applying the above function and storing in list
+        feats_list.append(feat_func(models[i], preprocs[i], array))
+
+    # features concatenating
+    final_feats = np.concatenate(feats_list, axis=-1)
+    # memory saving
+    del (feats_list, array)
+    gc.collect()
+
+    return final_feats
 
 # Models and preprocessors
 
@@ -132,22 +159,67 @@ models = [InceptionV3, InceptionResNetV2, Xception, ]
 preprocs = [inception_preprocessor, inc_resnet_preprocessor,
             xception_preprocessor, ]
 
+final_train_features = get_concat_features(get_features, models, preprocs, Xarr)
 
-# RETURNING CONCATENATED FEATURES USING MODELS AND PREPROCESSORS
-def get_concat_features(feat_func, models, preprocs, array):
-    print(f"Beggining extraction with {feat_func.__name__}\n")
-    feats_list = []
+#del(x_train, )
+gc.collect()
+print('Final feature maps shape', final_train_features.shape)
 
-    for i in range(len(models)):
-        print(f"\nStarting feature extraction with {models[i].__name__} using {preprocs[i].__name__}\n")
-        # applying the above function and storing in list
-        feats_list.append(feat_func(models[i], preprocs[i], array))
+from keras.callbacks import EarlyStopping
 
-    # features concatenating
-    final_feats = np.concatenate(feats_list, axis=-1)
-    # memory saving
-    del (feats_list, array)
-    gc.collect()
+EarlyStop_callback = keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=10, restore_best_weights=True, verbose=0)
 
-    return final_feats
+my_callback=[EarlyStop_callback]
 
+from sklearn.model_selection import StratifiedKFold
+
+splits = list(StratifiedKFold(n_splits=3, shuffle=True, random_state=10).split(final_train_features, Y))
+
+trained_models = []
+accuracy = []
+losses = []
+
+#Prepare And Train DNN model
+
+for i, (train_idx, valid_idx) in enumerate(splits):
+
+    print(f"\nStarting fold {i+1}\n")
+    x_train_fold = final_train_features[train_idx, :]
+    y_train_fold = Yarr_hot[train_idx, :]
+    x_val_fold = final_train_features[valid_idx]
+    y_val_fold = Yarr_hot[valid_idx, :]
+
+    dnn = keras.models.Sequential([
+        InputLayer(final_train_features.shape[1:]),
+        Dropout(0.7),
+        Dense(120, activation='softmax')
+    ])
+
+    dnn.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+    print("Training...")
+    #Train simple DNN on extracted features.
+    h = dnn.fit(x_train_fold, y_train_fold,
+                batch_size=128,
+                epochs=80,
+                verbose=0,
+                validation_data = (x_val_fold, y_val_fold),
+                callbacks=my_callback)  # max 95.07
+
+    print("Evaluating model ...")
+    model_res = dnn.evaluate(x_val_fold, y_val_fold)
+
+    accuracy.append(model_res[1])
+    losses.append(model_res[0])
+    trained_models.append(dnn)
+
+print('\n CV Score -')
+print(f"\nAccuracy - {sum(accuracy)/len(accuracy)}")
+print(f"\nLoss - {sum(losses)/len(losses)}")
+
+# SAVING RAM
+
+del(final_train_features, Y, Yarr_hot, Xarr)
+gc.collect()
